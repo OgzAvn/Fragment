adb62782712adb9342280e1836c51c3bec56f729
Oguz <oguzavanoglu007@gmail.com>
2024-03-18 17:56:21 +0300

    Fragment

---
+++ JavaFragment/app/src/main/java/com/oguz/javafragment/MainActivity.java
@@ -0,0 +1,40 @@
+package com.oguz.javafragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToFirst(View view){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout,firstFragment).commit(); add ile yapınca üst üste yazıyor.diğer fragment gitmiyor.
+        fragmentTransaction.replace(R.id.frame_layout,firstFragment).commit();
+
+    }
+
+    public void goToSecond(View view){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        //fragmentTransaction.add(R.id.frame_layout,secondFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,secondFragment).commit();
+
+    }
+}
